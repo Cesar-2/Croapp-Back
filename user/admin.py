170d9b44dde76b5317c5a495ae00cd0a0020aaaf
cesaraph <capimiento@utp.edu.co>
2021-10-31 15:24:53 -0500

    admin config

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(auth_admin.UserAdmin):
+    list_display = ["username", "first_name",
+                    'last_name', 'email', "is_superuser"]
+    search_fields = ["first_name", 'last_name', 'email', 'username']
