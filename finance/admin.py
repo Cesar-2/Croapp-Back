bd7dcc311c344191f7f6e7431d8279a110e6a40f
Cesar-2 <capimiento@utp.edu.co>
2021-12-06 23:40:41 -0500

    finance admin model

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from finance.models import Finance
 # Register your models here.
+
+
+@admin.register(Finance)
+class FinanceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+    list_filter = ('name',)
