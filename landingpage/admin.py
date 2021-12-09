72ef07ed1f9650d23443d452b73ebd7506596f1f
Cesar-2 <capimiento@utp.edu.co>
2021-12-08 20:46:59 -0500

    landing page

--- landingpage/admin.py
+++ landingpage/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from landingpage.models import LandingPage
+# Register your models here.
+
+
+@admin.register(LandingPage)
+class FinanceAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name')
+    list_filter = ('email',)
